1d39b45ba933f6ffcbca420094b7544b81cc7b16
- <->
2026-02-19 21:32:55 -0800

    Disambiguate Card from Hand.

--- dealer.py
+++ dealer.py
@@ -1,34 +1,49 @@
+class Card:
+    RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+    SUITS = ["c", "d", 'h', "s"]
 
+    def __init__(self, card):
+        self.rank = card[0]
+        self.suit = card[1]
+        self.value = Card.RANKS.index(self.rank)
 
-class Hand:
-    CARDS = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+    def __str__(self):
+        return f"{self.rank}{self.suit}"
+    
+    __repr__ = __str__
 
     @staticmethod
-    def strip_suits(cards):
-        return [card[0] for card in cards]
+    def generate_deck():
+        deck = {}
+        for rank in Card.RANKS:
+            for suit in Card.SUITS:
+                deck[f"{rank}{suit}"] = Card(rank, suit)
+        return deck
 
     @staticmethod
     def max(cards):
-
-        suitless = Hand.strip_suits(cards)
         max = -1
         index_of_max = -1
 
-        for i, card in enumerate(suitless):
-            if(Hand.CARDS.index(card) > max):
-                max = Hand.CARDS.index(card)
+        for i, card in enumerate(cards):
+            if(card.value > max):
+                max = card.value
                 index_of_max = i
 
         return cards[index_of_max]
 
     @staticmethod
     def compare(card_a, card_b):
-
         if(isinstance(card_a, list) and isinstance(card_b, list)):
             card_a = max(card_a)
             card_b = max(card_b)
 
-        return Hand.CARDS.index(card_a[0]) > Hand.CARDS.index(card_b[0])
+        return card_a.value > card_b.value
+
+
+class Hand:
+
+
         
 
     HANDS = \
@@ -75,11 +90,54 @@ class Hand:
             }
     }
 
+    @staticmethod
+    def make_consecutive(cards):
+        return sorted(cards, key = lambda card: Card.RANKS.index(card.rank))
 
     @staticmethod
+    def is_straight(cards):
+        joined = Card.RANKS[-1:].join()
+        ordered = Hand.make_consecutive(cards).join()
+        return ordered in joined
+
+    @staticmethod
+    def is_flush(cards):
+        suits = ()
+
+        for card in cards:
+            suits.add(card.suit)
+        
+        return len(suits) == 1
+
+    @staticmethod
+    def count(cards):
+        card_count = {}
+        for card in cards:
+            if card in card_count:
+                card_count[card] += 1
+            else:
+                card_count[card] = 1
+        return card_count
+
+    # It's gonna be sets of seven cards needing classification; 
+    # the five community cards and the two hole cards.
+    @staticmethod
     def classify(cards):
-        suitless = Hand.strip_suits(cards)
+        ordered = Hand.make_consecutive(cards)
+        suitless = [card.rank for card in ordered]
+        count = Hand.count(suitless)
+        setted = set(count.keys())
+        print(f"cards: {cards}\nordered: {ordered}\nsuitless: {suitless}\ncount: {count}\nsetted: {setted}")
+        """
         # Royal flush?
         if(set(suitless) == {'A', 'K', 'Q', 'J', 'T'}):
-            return 
+            return 'royal flush'
+        # Straight flush?
+        elif(Hand.is_straight(cards) and Hand.is_flush(cards))
+            return 'straight flush'
+        # Four of a kind?
+        """
 
+s = [Card("Ad"), Card("2d"), Card("5h"), Card("7d"), Card("2s"), Card("Tc")]
+print(Card("Ad"))
+Hand.classify(s)
